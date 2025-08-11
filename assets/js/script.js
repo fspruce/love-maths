f448beac3caf293ab6afec7b6b25ce971f26f402
fspruce <f.spruce@proton.me>
2025-08-11 10:55:20 +0100

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,19 +9,29 @@ document.addEventListener("DOMContentLoaded", function() {
         alert("You clicked Submit!");
       } else {
         let gameType = this.getAttribute("data-type");
-        alert(`You clicked ${gameType}`);
+        runGame(gameType);
       }
     })
   }
+  runGame("addition");
 })
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
   let num1 = Math.floor(Math.random() * 26);
   let num2 = Math.floor(Math.random() * 26);
+
+  switch (gameType) {
+    case 'addition':
+      displayAdditionQuestion(num1, num2);
+      break;
+    default: 
+      alert(`Unknown game type: ${gameType}`);
+      throw `Unknown gmae type: ${gameType}. Aborting!`;
+  }
 }
 
 function checkAnswer() {
@@ -40,8 +50,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+  document.getElementById('operand1').innerText = operand1;
+  document.getElementById('operand2').innerText = operand2;
+  document.getElementById('operator').innerText = "+";
 }
 
 function displaySubtractQuestion() {
