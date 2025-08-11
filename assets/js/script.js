fa58806717b7bb743ac3d84b53377d94cba24baf
fspruce <f.spruce@proton.me>
2025-08-11 11:02:06 +0100

    Create correct answer function for addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,8 +38,22 @@ function checkAnswer() {
 
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus, etc)
+ * directly from the dom, adn returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+  let operand1 = parseInt(document.getElementById('operand1').innerText);
+  let operand2 = parseInt(document.getElementById('operand2').innerText);
+  let operator = document.getElementById('operator').innerText;
 
+  switch (operator) {
+    case "+":
+      return [operand1 + operand2, "addition"]
+    default:
+      alert(`Unimplemented operator ${operator}`);
+      throw `Unimplemented operator ${operator}`;
+  }
 }
 
 function incrementScore() {
